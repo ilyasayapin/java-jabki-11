6a7edef0eac475c5f43da78438a9d8b65e6973ec
ilyasayapin <kite@nxt.ru>
2025-06-17 09:45:41 +0300

    Lesson 11. Homework

--- src/homework/HomeWorkTest.java
+++ src/homework/HomeWorkTest.java
@@ -1,6 +1,9 @@
 package homework;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,4 +13,23 @@ public class HomeWorkTest {
     public void safeDivide() {
         Assertions.assertEquals(2, HomeWork.safeDivide(2, 1));
     }
+
+    @Test
+    public void safeDivideNull() {
+        Assertions.assertNull(HomeWork.safeDivide(2, 0));
+    }
+
+    @Test
+    public void validateString() {
+        String str = "";
+        assertThrows(IllegalArgumentException.class, () -> HomeWork.validateString(str));
+    }
+
+    @Test
+    public void convertStrToNum() {
+        List<Integer> testList = HomeWork.convertStrToNum(List.of("10", "abc", "5"));
+        Assertions.assertEquals(List.of(10, 5), testList);
+    }
+
+
 }
